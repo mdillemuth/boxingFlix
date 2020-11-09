4b68ff2512cd54651628f712e2779b2ec073dfbf
Matt Dillemuth <mattdillemuth@gmail.com>
2020-11-08 21:53:10 -0500

    remove auth to build frontend

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -4,12 +4,12 @@ const Movies = require('./../../models/Movies'),
   router = express.Router();
 
 // Commented out for building the front-end
-const auth = passport.authenticate('jwt', { session: false });
+// const auth = passport.authenticate('jwt', { session: false });
 
 // @route    GET api/movies
 // @desc     Get list of all movies
 // @access   Private
-router.get('/', auth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const movies = await Movies.find();
     res.status(201).json(movies);
@@ -22,7 +22,7 @@ router.get('/', auth, async (req, res) => {
 // @route    GET api/movies/:MovieID
 // @desc     Get a specific movie by id
 // @access   Private
-router.get('/:MovieID', auth, async (req, res) => {
+router.get('/:MovieID', async (req, res) => {
   try {
     const movie = await Movies.find({ _id: req.params.MovieID });
     res.status(201).json(movie);
@@ -35,7 +35,7 @@ router.get('/:MovieID', auth, async (req, res) => {
 // @route    GET api/movies/:Genre
 // @desc     Get list of movies containing genre
 // @access   Private
-router.get('/genres/:Genre', auth, async (req, res) => {
+router.get('/genres/:Genre', async (req, res) => {
   try {
     const movie = await Movies.find({ 'Genre.Name': `${req.params.Genre}` });
     res.status(201).json(movie);
@@ -48,7 +48,7 @@ router.get('/genres/:Genre', auth, async (req, res) => {
 // @route    GET api/movies/:Director
 // @desc     Get data about a specific director
 // @access   Private
-router.get('/directors/:Director', auth, async (req, res) => {
+router.get('/directors/:Director', async (req, res) => {
   try {
     const movie = await Movies.findOne({
       'Director.Name': `${req.params.Director}`,
