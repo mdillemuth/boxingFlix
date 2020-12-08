3f3d15a2090e558b39eec8411f7c99b4c4bc6e03
Matt Dillemuth <mattdillemuth@gmail.com>
2020-12-07 23:19:43 -0500

    Add cors() and corsOptions to /users in index

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const cors = require('cors');
 
 var corsOptions = {
   origin: '*',
-  preflightContinue: true,
   allowHeaders: 'Authorization',
+  preflightContinue: true,
 };
 
 app.use(cors(corsOptions));
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 });
 app.use('/api/login', require('./routes/api/auth'));
 app.use('/api/movies', require('./routes/api/movies'));
-app.use('/api/users', require('./routes/api/users'));
+app.use('/api/users', cors(corsOptions), require('./routes/api/users'));
 
 // Port listening
 const port = process.env.PORT || 8181;
